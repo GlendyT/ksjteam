7c9d1da064d6d37a708baa9f9da60c5615a32c7b
GlendyT <glendytuyuc@gmail.com>
2024-10-18 10:55:38 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-blue-600 text-2xl">Come Play with Kim Seokjin</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
