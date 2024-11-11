b8c2bb716cf21bf4fe9cc23071a64bd89b4daebc
GlendyT <glendytuyuc@gmail.com>
2024-11-11 17:33:44 -0600

    test2

--- src/components/Imagenes.tsx
+++ src/components/Imagenes.tsx
@@ -1,9 +1,11 @@
 import jin from "../assets/fish1.webp";
+import jin2 from "../assets/fish2.webp"
 
 const Imagenes = () => {
   return (
     <div className="min-h-screen flex flex-row items-center justify-center">
       <img src={jin} alt="imagen de un pez" height={150} width={50} />
+      <img src={jin2} alt="imagen de un pez" height={150} width={50} />
     </div>
   );
 };
