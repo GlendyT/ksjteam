e34b1b31e0a2b3d954cc44cc947b1449e94f1028
Glendy Tuyuc <glendytuyuc@gmail.com>
2024-12-03 16:26:50 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-//import { Fishing } from "./components/Fishing";
-import ScheduledImage from "./components/ScheduledImage";
+import { Fishing } from "./components/Fishing";
+import Waves from "./waves/Waves";
+
 
 //import Waves from "./waves/Waves";
 
 function App() {
   return (
     <div className="bg-[#2b74ce] flex flex-col min-h-screen ">
-      {/* <div className="flex-1 max-sm:flex">
+      <div className="flex-1 max-sm:flex">
         <Fishing />
       </div>
-      <Waves /> */}
-      <ScheduledImage />
+      <Waves />
+
     </div>
   );
 }
