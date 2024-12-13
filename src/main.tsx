2da81e8d4d64822dba5e67ff805a61a718981396
Glendy Tuyuc <glendytuyuc@gmail.com>
2024-12-12 22:29:00 -0600

    memory game3

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { FishProvider } from "./context/FishProvider";
-import { RevealProvider } from "./context/RevealProvider";
 import { FlipProvider } from "./context/FlipProvider";
 
 createRoot(document.getElementById("root")!).render(
-
-  <FishProvider>
-    <RevealProvider>
-      <FlipProvider>
-        <App /></FlipProvider>
-    </RevealProvider>
-  </FishProvider>
-
+  <FlipProvider>
+    <App />
+  </FlipProvider>
 );
